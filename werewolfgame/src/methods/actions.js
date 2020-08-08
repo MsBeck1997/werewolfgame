bc3a4d7f000d9edcc3f0001bbcea7bc2ac451b97
MsBeck1997 <sarah.lizzie.beck@gmail.com>
2020-08-07 19:12:46 -0600

    minor changes for better layouts and organization

--- werewolfgame/src/methods/actions.js
+++ werewolfgame/src/methods/actions.js
@@ -2,14 +2,20 @@ export const Actions = {
 
     hunt: function() {
         // Update time in statusBar
+
         // Calculate a random percent rounded
-        // if 0-10, epic failure
-        // 10-49, failure
-        // 50-89, success
-        // if 90-100, epic success
+        let roll = Math.floor(Math.random() * 100)
+          if (roll < 11) {
+            console.log('epic failure')
+          } else if (roll < 50) {
+            console.log('Failure')
+          } else if (roll < 90) {
+            console.log('Success')
+          } else if (roll < 101) {
+            console.log('Epic Success')
+          }
         // Push changes to stats in statusBar
         // Publish result in log box
-        console.log('test')
     },
 
 
